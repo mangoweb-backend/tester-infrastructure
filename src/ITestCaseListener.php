b2b0dcc9d1c513ae77f9bcced0a62e1e0feda5c6
Jan Trejbal <jan.trejbal@scalesoft.cz>
2018-03-17 18:35:00 +0100

    Extend testCaseListener with method reflection and applicationContainer

--- src/ITestCaseListener.php
+++ src/ITestCaseListener.php
@@ -2,9 +2,11 @@
 
 namespace Mangoweb\Tester\Infrastructure;
 
+use Nette\DI\Container;
+
 interface ITestCaseListener
 {
-	public function setUp(TestCase $testCase): void;
+	public function setUp(TestCase $testCase, Container $applicationContainer, \ReflectionMethod $testMethod): void;
 
-	public function tearDown(TestCase $testCase): void;
+	public function tearDown(TestCase $testCase, Container $applicationContainer, \ReflectionMethod $testMethod): void;
 }
